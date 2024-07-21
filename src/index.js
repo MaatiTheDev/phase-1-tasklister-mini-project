9c5130a586971eb9c12ca926e38d72e5f60cabf0
felix maati <maatifelix001@gmail.com>
2024-07-21 15:01:35 +0300

    event listing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const task = taskInput.value.trim();
+
+    if (task !== "") {
+      addTaskToList(task);
+      taskInput.value = "";
+    }
+  });
+});
+
+function addTaskToList(task) {
+  const tasksList = document.getElementById("tasks");
+
+  const li = document.createElement("li");
+  li.textContent = task;
+
+  tasksList.appendChild(li);
+}
